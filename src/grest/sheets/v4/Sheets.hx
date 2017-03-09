17d685dbed959d9c4be189b40f9a7e1e041788ff
Kevin Leung <kevinresol@gmail.com>
2017-03-09 17:24:00 +0800

    Use generated API

--- src/grest/sheets/v4/Sheets.hx
+++ src/grest/sheets/v4/Sheets.hx
@@ -5,17 +5,12 @@ import tink.web.proxy.Remote;
 import tink.url.Host;
 
 class Sheets {
-	public static var instance(get, null):Remote<Api>;
+	public static var instance(get, null):Remote<grest.sheets.v4.api.Sheets>;
 	static function get_instance() {
 		if(instance == null) {
-			instance = new Remote<Api>(new SecureSocketClient(), new RemoteEndpoint(new Host('sheets.googleapis.com')));
+			instance = new Remote<grest.sheets.v4.api.Sheets>(new SecureSocketClient(), new RemoteEndpoint(new Host('sheets.googleapis.com')));
 		}
 		return instance;
 	}
 	
-}
-
-interface Api {
-	@:sub('/v4/spreadsheets')
-	var spreadsheets:Spreadsheets;
 }
